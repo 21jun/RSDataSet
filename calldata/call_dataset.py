7e971302cd52f33e808842cd4d9bd861c04300f5
21jun <21jun7654@gmail.com>
2019-09-10 22:11:05 +0900

    add multiple datasets

--- calldata/call_dataset.py
+++ calldata/call_dataset.py
@@ -1,26 +1,49 @@
 import numpy as np
-from pathlib import Path 
-
+import pandas as pd
 
 class RSDataSet:
     def __init__(self, filepath):
+        self.name = self.__class__.__name__
         self.filepath = filepath
-        self.x = []
+        self.user_num, self.item_num, self.x = 0, 0, []
+        self.mapping_table = {} # user_ids, item_ids
+
         self._load_dataset()
 
+        self._verify_dataset()
+    
+    # 데이터셋 마다 다른 로드 구현이 필요
     def _load_dataset(self):
         pass
     
+    # 인덱스로 해당 유저의 id 를 검색
+    def get_user_id(self, i):
+        print(f'user index = {i}, actual user id = {self.mapping_table["user_ids"][i]}')
+    
+    # 인덱스로 해당 아이템의 id 를 검색
+    def get_item_id(self, i):
+        print(f'item index = {i}, actual item id = {self.mapping_table["item_ids"][i]}')
+        
+    # 생성한 데이터셋이 정상적으로 로드되었는지 검증
+    def _verify_dataset(self):
+        assert self.user_num > 0
+        assert self.item_num > 0
+    
+    # 생성한 인스턴스에서 바로 User-Item Rating Matrix 로 접근 가능
     def __getitem__(self, i):
         return self.x[i]
 
     def __len__(self):
         return len(self.x)
 
+    # 인스턴스의 정보 확인용
     def __repr__(self):
-        pass
+        return \
+        f'\n{self.name}\n' +\
+        f'number of users : {self.user_num}\n' +\
+        f'number of items : {self.item_num}'
 
-
+# 데이터로더에 데이터셋을 등록하면 원하는 배치사이즈 만큼씩 iteration 가능
 class RSDataLoader():
     def __init__(self, ds: RSDataSet, bs=1):
         self.ds, self.bs = ds, bs
@@ -36,13 +59,15 @@ class MovieLensDataSet(RSDataSet):
     
     def _load_dataset(self):
 
-        user_max=0
-        item_max=0
+        user_max, item_max = 0, 0
         with open(self.filepath) as f:
             for line in f:
                 user_id, item_id, _, _ = line.split('\t')
                 user_max = max([user_max, int(user_id)])
                 item_max = max([item_max, int(item_id)])
+
+        self.user_num = user_max
+        self.item_num = item_max
 
         matrix = [[0 for col in range(item_max)] for row in range(user_max)]
 
@@ -52,3 +77,71 @@ class MovieLensDataSet(RSDataSet):
                 matrix[int(user_id)-1][int(item_id)-1] = int(rating)
 
         self.x = matrix
+
+        user_ids = [id for id in range(1, self.user_num)]
+        item_ids = [id for id in range(1, self.item_num)]
+
+        self.mapping_table = {'user_ids': user_ids, 'item_ids': item_ids}
+
+class BookCrossingDataSet(RSDataSet):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+    
+    def _load_dataset(self):
+
+        df = pd.read_csv(self.filepath, sep=';')
+        df = df.iloc[:1000, :]  # 앞쪽 1000개의 row 만 사용
+        # 전체 데이터로 메트릭스 만들면 메모리 초과 (over 16gb)
+
+        matrix = df.pivot(index='User-ID', columns='ISBN', values='Book-Rating')
+        self.user_num, self.item_num = matrix.shape
+        self.x = matrix.values.tolist()
+
+        user_ids = matrix.index.values.tolist()
+        item_ids = matrix.columns.values.tolist()
+
+        self.mapping_table = {'user_ids': user_ids, 'item_ids': item_ids}
+
+class JesterDataSet(RSDataSet):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def _load_dataset(self):
+    
+        df = pd.read_excel(self.filepath, header=None)
+        df= df.iloc[:, 1:]  # 첫번째 컬럼은 필요없기에 제거
+        self.user_num , self.item_num = df.shape
+        matrix = df
+        self.x = matrix.values.tolist()
+
+        user_ids = matrix.index.values.tolist()
+        item_ids = matrix.columns.values.tolist()
+
+        self.mapping_table = {'user_ids': user_ids, 'item_ids': item_ids}
+
+class EachMovieDataSet(RSDataSet):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+    
+    def _load_dataset(self):
+        df = pd.read_csv(self.filepath, sep=' ', header=None)
+        df.columns = ['item_id', 'user_id', 'rating']
+        matrix = df.pivot(index='user_id', columns='item_id', values='rating')
+
+        self. user_num, self.item_num = matrix.shape
+        self.x = matrix.values.tolist()
+
+        user_ids = matrix.index.values.tolist()
+        item_ids = matrix.columns.values.tolist()
+
+        self.mapping_table = {'user_ids': user_ids, 'item_ids': item_ids}
+
+
+class NetflixDataSet(RSDataSet):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def _load_dataset(self):
+        pass
+
+
--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from calldata import call_dataset
+from calldata.call_dataset import *
 
 if __name__ == '__main__':
     
-    MovieLens = call_dataset.MovieLensDataSet('./dataset/ml-100k/u.data')
-    print(MovieLens[307][0])
-   
-    a = call_dataset.RSDataLoader(MovieLens, 3)
+    MovieLens = MovieLensDataSet('./dataset/ml-100k/u.data')
+    print(MovieLens)
+    print(f'MovieLens[195][241] = {MovieLens[195][241]}')
 
+    Jester = JesterDataSet('./dataset/jester-data-1/jester-data-1.xls')
+    print(Jester)
+    print(f'Jester[0][0] = {Jester[0][0]}')
+    Jester.get_user_id(1343)
+    Jester.get_item_id(5)
 
-    for i in a:
-        print(i)
+    BookCrossing = BookCrossingDataSet('./dataset/BX-CSV-Dump/BX-Book-Ratings.csv')
+    print(BookCrossing)
+    print(f'BookCrossing[0][1] = {BookCrossing[0][1]}')
+    BookCrossing.get_user_id(140)
+    BookCrossing.get_item_id(750)
+
+    EachMovie = EachMovieDataSet('./dataset/rec-eachmovie/rec-eachmovie.edges')
+    print(EachMovie)
+    print(f'EachMovie[0][0] = {EachMovie[0][0]}')
+    EachMovie.get_user_id(14553)
+    EachMovie.get_item_id(120)
+
+    # DataLoader = RSDataLoader(MovieLens)
+
+    # for i in DataLoader:
+    #     print(i[0][0])
