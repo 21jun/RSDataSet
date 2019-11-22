33cd41a478e6e325ce1dff36ac36ee7db83ba93e
21jun <21jun7654@gmail.com>
2019-11-23 03:53:13 +0900

    refactor  intent, line

--- similarity/similarity.py
+++ similarity/similarity.py
@@ -1,24 +1,23 @@
 import numpy as np
 from abc import abstractmethod
 
-class Similarity():
+
+class Similarity:
 
     def __init__(self):
-        
-        self.name = self.__class__.__name__      
+        self.name = self.__class__.__name__
         self.m = None
         self.sim_ = None
         # 테스트용
-        self.test_matrix = np.array([[4,1,1,4],
-                                    [0,4,2,0],
-                                    [2,0,4,5],
-                                    [1,4,0,1]])
+        self.test_matrix = np.array([[4, 1, 1, 4],
+                                     [0, 4, 2, 0],
+                                     [2, 0, 4, 5],
+                                     [1, 4, 0, 1]])
 
     def set_base(self, base):
-        
         options = ['user', 'item', 'test']
         assert base.lower() in options
-        if base is 'test':  
+        if base is 'test':
             self.m = self.test_matrix
             return
 
@@ -26,9 +25,9 @@ class Similarity():
 
     # 클래스마다 별도의 구현 필요
     @abstractmethod
-    def fit(self):
+    def fit(self, dataset, base='user'):
         pass
-    
+
 
 class CosineSimilarity(Similarity):
 
@@ -53,15 +52,16 @@ class PearsonCorrelationCoefficient(Similarity):
     def fit(self, dataset, base='user'):
         self.m = np.array(dataset[:])
         self.set_base(base)
-        # self.sim_ = np.corrcoef(self.m)
+
         x = self.m
         mean_x = x.mean(axis=1)
-        xm = np.subtract(x, mean_x.reshape(-1,1))
+        xm = np.subtract(x, mean_x.reshape(-1, 1))
 
         d = xm @ xm.T
         norm = np.linalg.norm(xm, axis=1)
         norm = norm.reshape(norm.shape[0], -1)
         self.sim_ = d / (norm * norm.T)
+
 
 class JaccardSimilarity(Similarity):
 
@@ -71,14 +71,14 @@ class JaccardSimilarity(Similarity):
     def fit(self, dataset, base='user'):
         self.m = np.array(dataset[:])
         self.set_base(base)
-        x = np.where(np.isnan(self.m),0, 1)
-        
+        x = np.where(np.isnan(self.m), 0, 1)
+
         sim = [0 for x in range(x.shape[0])]
         for i in range(x.shape[0]):
-            if i%1000 == 0: print(i," / ",x.shape[0])
+            if i % 1000 == 0: print(i, " / ", x.shape[0])
             intersection = np.logical_and(x[i], x)
             union = np.logical_or(x[i], x)
-            jac = intersection.sum(1) / union.sum(1)           
-            sim[i]=jac
+            jac = intersection.sum(1) / union.sum(1)
+            sim[i] = jac
 
         self.sim_ = np.array(sim)
